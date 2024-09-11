0f17dae1b90615a612e8a89229d478818ad24cce
Rupesh Pandey <rupeshpandey432@gmail.com>
2024-09-11 15:13:28 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DocumentFormComponent } from './components/document-form/document-form.component';
+import { DocumentDashboardComponent } from './components/document-dashboard/document-dashboard.component';
+import { EditDocumentComponent } from './components/edit-document/edit-document.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'document/edit/:id', component: EditDocumentComponent },
+  { path: 'add-document', component: DocumentFormComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DocumentDashboardComponent, canActivate: [AuthGuard] },
+  
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DocumentDashboardComponent } from './components/document-dashboard/document-dashboard.component';
+import { DocumentFormComponent } from './components/document-form/document-form.component';
+import { DocumentViewerComponent } from './components/document-viewer/document-viewer.component';
+import { EditDocumentComponent } from './components/edit-document/edit-document.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentDashboardComponent,
+    DocumentFormComponent,
+    DocumentViewerComponent,
+    EditDocumentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
