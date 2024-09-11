77e923f45b5201e5869d51190a81298adc2397e1
Rupesh Pandey <rupeshpandey432@gmail.com>
2024-09-11 15:01:39 +0530

    update

---
+++ src/app/components/document-dashboard/document-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-dashboard',
+  templateUrl: './document-dashboard.component.html',
+  styleUrls: ['./document-dashboard.component.css']
+})
+export class DocumentDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
